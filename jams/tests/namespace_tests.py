84f1e60e0d0c308ef9fdd052c4830a290411069f
Brian McFee <brian.mcfee@nyu.edu>
2015-05-26 16:59:42 -0400

    added lyrics namespace validation

--- jams/tests/namespace_tests.py
+++ jams/tests/namespace_tests.py
@@ -4,16 +4,15 @@
 
 import numpy as np
 
-from nose.tools import eq_, raises
+from nose.tools import raises
 from jsonschema import ValidationError
-import six
 
-import jams
+from jams import Annotation
 
 
 def test_ns_time_valid():
 
-    ann = jams.Annotation(namespace='beat')
+    ann = Annotation(namespace='beat')
 
     for time in np.arange(5.0, 10.0):
         ann.append(time=time, duration=0.0, value=None, confidence=None)
@@ -25,7 +24,7 @@ def test_ns_time_invalid():
 
     @raises(ValidationError)
     def __test(data):
-        ann = jams.Annotation(namespace='beat')
+        ann = Annotation(namespace='beat')
         ann.append(**data)
 
         ann.validate()
@@ -36,10 +35,11 @@ def test_ns_time_invalid():
     # Check bad duration
     yield __test, dict(time=1, duration=-1)
 
+
 def test_ns_beat_valid():
 
     # A valid example
-    ann = jams.Annotation(namespace='beat')
+    ann = Annotation(namespace='beat')
     
     for time in np.arange(5.0):
         ann.append(time=time, duration=0.0, value=1, confidence=None)
@@ -53,7 +53,7 @@ def test_ns_beat_valid():
 @raises(ValidationError)
 def test_ns_beat_invalid():
 
-    ann = jams.Annotation(namespace='beat')
+    ann = Annotation(namespace='beat')
     
     for time in np.arange(5.0):
         ann.append(time=time, duration=0.0, value='foo', confidence=None)
@@ -63,7 +63,7 @@ def test_ns_beat_invalid():
 
 def test_ns_beat_position_valid():
 
-    ann = jams.Annotation(namespace='beat_position')
+    ann = Annotation(namespace='beat_position')
 
     ann.append(time=0, duration=1.0, value=dict(position=1,
                                                 measure=1,
@@ -77,7 +77,7 @@ def test_ns_beat_position_invalid():
 
     @raises(ValidationError)
     def __test(value):
-        ann = jams.Annotation(namespace='beat_position')
+        ann = Annotation(namespace='beat_position')
         ann.append(time=0, duration=1.0, value=value)
         ann.validate()
 
@@ -112,4 +112,23 @@ def test_ns_beat_position_invalid():
         value = good_dict.copy()
         del value[field]
         yield __test, value
+
+    # And test non-object values
+    yield __test, None
+
+
+def test_ns_lyrics():
+
+    def __test(lyric):
+        ann = Annotation(namespace='lyrics')
+
+        ann.append(time=0, duration=1, value=lyric)
+
+        ann.validate()
+
+    for line in ['Check yourself', u'before you wreck yourself']:
+        yield __test, line
+
+    for line in [23, None]:
+        yield raises(ValidationError)(__test), line
 
