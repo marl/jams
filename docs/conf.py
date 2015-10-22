efd95a53680eb5f39f52cfc3747e11be2b53aff6
Brian McFee <brian.mcfee@nyu.edu>
2015-10-22 15:26:01 -0400

    updating mocks

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['jsonschema', 'pandas', 'numpy']
+MOCK_MODULES = ['jsonschema', 'mir_eval', 'pandas', 'numpy']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
