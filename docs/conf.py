c336bed53942b9e24849e7fcbba78bc8ecd6e400
Brian McFee <brian.mcfee@nyu.edu>
2015-10-22 14:43:51 -0400

    updated sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 needs_sphinx = '1.2'
