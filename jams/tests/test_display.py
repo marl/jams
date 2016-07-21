99befe10d9e538527b940fefba891e565b884df2
Brian McFee <brian.mcfee@nyu.edu>
2016-07-21 16:06:26 -0400

    added coverage for display cases

--- jams/tests/test_display.py
+++ jams/tests/test_display.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import numpy as np
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -33,9 +35,29 @@ def test_display_multi():
 
     jam = jams.JAMS()
     jam.annotations.append(jams.Annotation(namespace='beat'))
+    jams.display.display_multi(jam.annotations)
+
+
+def test_display_multi_multi():
+
+    jam = jams.JAMS()
+    jam.annotations.append(jams.Annotation(namespace='beat'))
     jam.annotations.append(jams.Annotation(namespace='chord'))
 
     jams.display.display_multi(jam.annotations)
+
+
+def test_display_pitch_contour():
+
+    ann = jams.Annotation(namespace='pitch_hz', duration=5)
+
+    values = np.arange(100, 200)
+    times = np.linspace(0, 2, num=len(values))
+
+    for t, v in zip(times, values):
+        ann.append(time=t, value=v, duration=0)
+
+    jams.display.display(ann)
 
 
 @raises(jams.ParameterError)
