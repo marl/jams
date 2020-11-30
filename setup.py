ccd35ba7f7774fb45a3715766734b9f8103f0a4e
Hendrik Schreiber <hs@tagtraum.com>
2020-11-30 12:58:04 +0100

    Removed target Python 3.9, as it currently fails (sphinx).
    UnsatisfiableError: The following specifications were found to be in conflict:
    - python 3.9*
    - sphinx -> docutils >=0.11 -> python 3.5* -> libffi >=3.2.1,<3.3a0
    - sphinx -> docutils >=0.11 -> python 3.5* -> openssl >=1.0.2n,<1.0.3a
    - sphinx -> docutils >=0.11 -> python 3.5* -> readline >=7.0,<8.0a0

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8"
     ],
     keywords='audio music json',
     license='ISC',
